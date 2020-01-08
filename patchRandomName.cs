e478472f8c47a37e56bab1d3aa3373122b4d0c31
Kristen <43615563+K10ForTheWin@users.noreply.github.com>
2020-01-08 09:21:26 -0600

    initial commit

---
+++ patchRandomName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameGenerator
+{
+    class patchRandomName
+    {
+        internal static void Postfix(ref string __source)
+        {
+            source = "Meowdy";
+        }
+    }
+}
